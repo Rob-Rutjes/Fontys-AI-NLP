f92bde00bedc2e75e0fdf37e86099a862421bf17
Rob Rutjes <rob.rutjes@hotmail.com>
2023-11-22 15:26:10 +0100

    add - comments to the code in main.py and tools.py

--- main.py
+++ main.py
@@ -1,52 +1,63 @@
-from tempfile import NamedTemporaryFile
 import streamlit as st
-from langchain.agents import initialize_agent
+
+from tempfile import NamedTemporaryFile
 from langchain.chat_models import ChatOpenAI
+from langchain.agents import initialize_agent
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 from tools import ImageCaptionTool, ObjectDetectionTool
 
+# Initialize the tools to be used by the agent from the tools.py file
 tools = [ImageCaptionTool(), ObjectDetectionTool()]
 
 conversation_memory = ConversationBufferWindowMemory(
-    memory_key="chat_history",
-    k=5,
-    return_messages=True
+    memory_key="chat_history",  # Key to be used to store the memory in the agent's memory
+    k=5,                        # Number of messages to be stored in the memory
+    return_messages=True        # If true, will return the messages stored in the memory when the agent is initialized
 )
 
 llm = ChatOpenAI(
-    openai_api_key='sk-XlXfFVURP9Ka......',
-    temperature=0,
-    model_name='gpt-3.5-turbo'
+    openai_api_key='sk-XlXfFVURP9Ka......',  # OpenAI API key, you can get one from https://platform.openai.com/
+    temperature=0,                           # Temperature for the generation (how much the model should be creative)
+    model_name='gpt-3.5-turbo'               # OpenAI Model name
 )
 
 agent = initialize_agent(
-    agent="chat-conversational-react-description",
-    tools=tools,
-    llm=llm,
-    max_iterations=5,
-    verbose=True,
-    memory=conversation_memory,
-    early_stopping_method="generate"
+    agent="chat-conversational-react-description",  # Agent name
+    tools=tools,                                    # Tools to be used by the agent
+    llm=llm,                                        # Language model to be used by the agent
+    max_iterations=5,                               # Maximum number of iterations
+    verbose=True,                                   # Verbose mode, if true, will print the agent's messages to the console
+    memory=conversation_memory,                     # History memory to be used by the agent
+    early_stopping_method="generate"                # Early stopping method, if generate, will stop when the agent generates a message
 )
 
+# Streamlit app title and header
 st.title('As a question about an image')
-
 st.header('Upload your image below:')
 
+# Upload button for the image
 uploaded_file = st.file_uploader("", type=["jpeg", "jpg", "png"])
 
+# If an image is uploaded
 if uploaded_file:
+    # Display the image
     st.image(uploaded_file, use_column_width=True)
 
+    # Text input for the question
     question = st.text_input('Ask a question about the image')
 
+    # Save file to a temporary file
     with NamedTemporaryFile(dir='.') as f:
         f.write(uploaded_file.getbuffer())
         image_path = f.name
 
+        # If a question is asked
         if question and question != '':
+            # Send the question and image path to the agent and await a response
             response = agent.run('{}, this is the image path: {}').format(question, image_path)
 
+            # Display a spinner while the agent is working
             with st.spinner(text="IN PROGRESS..."):
+                # Display the response
                 st.write(response)
--- tools.py
+++ tools.py
@@ -3,32 +3,34 @@ from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageP
 from PIL import Image
 import torch
 
-
+# This is a tool that can be used by the agent to describe an image
 class ImageCaptionTool(BaseTool):
     name = "Image Captioning"
     description = "Use this tool when given the path to an image that you would like to be described. " \
                   "It will return a simple caption describing the image."
 
     def _run(self, img_path):
-        image = Image.open(img_path).convert('RGB')
+        image = Image.open(img_path).convert('RGB')  # Convert the image to RGB
 
-        model_name = "Salesforce/blip-image-captioning-large"
-        device = "cuda"  # when no GPU available, change to "cpu"
+        model_name = "Salesforce/blip-image-captioning-large"  # Model name from huggingface.co/models
+        device = "cuda"                                        # Device to run the model on, "cuda" for GPU, "cpu" for CPU
 
-        processor = BlipProcessor.from_pretrained(model_name)
-        model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+        processor = BlipProcessor.from_pretrained(model_name)                        # Initialize the processor
+        model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)  # Initialize the model
 
-        inputs = processor(image, return_tensors="pt").to(device)
-        output = model.generate(**inputs, max_new_tokens=20)
+        inputs = processor(image, return_tensors="pt").to(device)  # Process the image
+        output = model.generate(**inputs, max_new_tokens=20)       # Generate a caption for the image
 
-        caption = processor.decode(output[0], skip_special_tokens=True)
+        caption = processor.decode(output[0], skip_special_tokens=True)  # Decode the caption
 
+        # Return the caption
         return caption
 
     def _arun(self, query: str):
         raise NotImplementedError("This tool does not support async running.")
 
 
+# This is a tool that can be used by the agent to detect objects in an image
 class ObjectDetectionTool(BaseTool):
     name = "Object Detection"
     description = "Use this tool when given the path to an image that you would like to detect objects in. " \
@@ -36,22 +38,24 @@ class ObjectDetectionTool(BaseTool):
                   "label score"
 
     def _run(self, img_path):
-        image = Image.open(img_path).convert('RGB')
+        image = Image.open(img_path).convert('RGB') # Convert the image to RGB
 
-        processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-        model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+        processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm") # Initialize the processor
+        model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm") # Initialize the model
 
-        inputs = processor(images=image, return_tensors="pt")
-        outputs = model(**inputs)
+        inputs = processor(images=image, return_tensors="pt")  # Process the image
+        outputs = model(**inputs)                              # Detect objects in the image
 
         target_sizes = torch.tensor([image.size[::-1]])
-        results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+        results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]  # Post process the results
 
+        # Return the detections in a string with labels and scores
         detections = ""
         for score, label in zip(results["scores"], results["labels"]):
             detections += ' {}'.format(model.config.id2label[int(label)])
             detections += ' {}\n'.format(float(score))
 
+        # Return the detections
         return detections
 
     def _arun(self, query: str):
